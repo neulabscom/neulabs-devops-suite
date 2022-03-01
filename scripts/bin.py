6e57570859a7ddd3808d8556492dc06de58c7785
FabrizioCafolla <developer@fabriziocafolla.com>
2022-03-01 17:08:34 +0100

    fix

--- scripts/bin.py
+++ scripts/bin.py
@@ -7,12 +7,12 @@ import pathlib
 def _scripts():
     import shutil
 
-    if os.getenv("NEULABS_BIN_PATH") is None:
-        print("NEULABS_BIN_PATH not found in ENV vars")
+    dest_dir = os.getenv("NEULABS_BIN_PATH", pathlib.Path(os.getenv("HOME"), ".neulabs", "bin"))
+    if not os.path.isdir(dest_dir):
+        print(f"NEULABS_BIN_PATH not found in {dest_dir}")
         return 1
 
     source_dir = pathlib.Path(pathlib.Path(__file__).parent.resolve(), "..", "bin") 
-    dest_dir = pathlib.Path(os.getenv("NEULABS_BIN_PATH"))
     
     for file_name in os.listdir(source_dir):
         source = pathlib.Path(source_dir, file_name)
