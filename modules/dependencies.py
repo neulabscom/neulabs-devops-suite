4c4d3d2f7a401a83346dcb2113b35ecf02a7b01c
FabrizioCafolla <developer@fabriziocafolla.com>
2022-03-25 11:48:58 +0100

    fix

--- modules/dependencies.py
+++ modules/dependencies.py
@@ -121,7 +121,7 @@ DEPS = {
             ),
             'darwin': (
                 'curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"',
-                'sudo installer -pkg AWSCLIV2.pkg -target'
+                'sudo installer -pkg AWSCLIV2.pkg -target /'
             ),
         },
     },
